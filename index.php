642e7e07373c14aa1e8e74f518990e81ce9b7cb1
Luz Septimo <lseptimo.0110@gmail.com>
2018-04-26 12:18:03 -0500

    Update index.php

--- index.php
+++ index.php
@@ -11,30 +11,18 @@
       echo $welcome;
       ?>
       </h1>
-    <p><strong>Things you can do:</strong>
+    <p><strong>¿Qué puedes hacer con esta app?</strong>
       <?php
-        $things = array("Talk to databases",
-        "Send cookies", "Evaluate form data",
-        "Build dynamic webpages");
+        $things = array("Calcular el costo de viaje entre lugares turísticos de Panamá",
+        "Encontrar contactos de las líneas de transporte de Panamá", "Encontrar lugares para visitar",
+        "Sugerir ideas para esta app!");
         foreach ($things as $thing) {
             echo "<li>$thing</li>";
         }
         
         unset($thing);
       ?>
-    </p>
-    <p><strong>This jumbled sentence will change every time you click Submit!<strong></p>
-    <p>
-      <?php
-        $words = array("the ", "quick ", "brown ", "fox ",
-        "jumped ", "over ", "the ", "lazy ", "dog ");
-        shuffle($words);
-        foreach ($words as $word) {
-            echo $word;
-        };
-        
-        unset($word);
-      ?>
-    </p>
+    
+    
   </body>
 </html>
